ad3262733d982e51575164d66f4acdcf604c73d3
ibrahimx012 <ibrahimreshid7@gmail.com>
2023-08-05 17:32:46 +0400

    Create App

---
+++ src/test/java/com/company/RegistrationAndLoginApplicationTests.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationAndLoginApplicationTests {
+
+}
